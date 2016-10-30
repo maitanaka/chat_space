90a3279ed4b7349a86dde78362e7bd38c956f12b
Mai Tanaka <bearbearbear9@gmail.com>
2016-10-30 19:08:27 +0900

    create messages_controller and edit model for validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,26 @@
 class MessagesController < ApplicationController
 
   def index
+    @message = Message.new
+    @messages = Message.all
   end
+
+  def new
+  end
+
+  def create
+    @message = Message.new(create_messages)
+    if @message.save
+      redirect_to root_path
+    else
+      flash.now[:alert] = @message.errors.full_messages.join(",")
+      render :index
+    end
+  end
+
+  private
+    def create_messages
+      params.require(:message).permit(:text)
+    end
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
+
+  validates_presence_of :text
+
 end
